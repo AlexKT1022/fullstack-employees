6e91327391fcf8d857852aadc7ba70ee104d83a5
AlexKT1022 <alexkt1022@gmail.com>
2025-08-29 16:05:47 -0400

    completed post, put, and patch functionality

--- api/employees.js
+++ api/employees.js
@@ -1,6 +1,12 @@
 import express from 'express';
 
-import { getEmployee, getEmployees } from '#db/queries/employees';
+import {
+  createEmployee,
+  deleteEmployee,
+  getEmployee,
+  getEmployees,
+  updateEmployee,
+} from '#db/queries/employees';
 
 const router = express.Router();
 
@@ -17,6 +23,41 @@ router.get('/:id', async (req, res, next) => {
   if (!employee) res.sendStatus(404);
 
   res.send(employee);
+});
+
+router.post('/', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, birthday, salary } = req.body;
+
+  if (!id || !name || !birthday || !salary) res.sendStatus(500);
+
+  const newEmployee = await createEmployee({ id, name, birthday, salary });
+
+  if (!newEmployee) res.sendStatus(404);
+
+  res.send(newEmployee);
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, birthday, salary } = req.body;
+
+  if (!id || !name || !birthday || !salary) res.sendStatus(500);
+
+  const updatedEmployee = await updateEmployee({ id, name, birthday, salary });
+
+  if (!updatedEmployee) res.sendStatus(404);
+
+  res.send(updatedEmployee);
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const deletedEmployee = await deleteEmployee(id);
+
+  if (!deletedEmployee) res.sendStatus(404);
+
+  res.send(deletedEmployee);
 });
 
 export default router;
--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,20 +1,5 @@
 import db from '#db/client';
 
-/** @returns the employee created according to the provided details */
-export const createEmployeeQuery = async ({ name, birthday, salary }) => {
-  const sql = `
-    insert into employees(name, birthday, salary)
-    values ($1, $2, $3)
-    returning *
-  `;
-  const values = [name, birthday, salary];
-  const res = await db.query(sql, values);
-
-  return res.rows[0];
-};
-
-// === Part 2 ===
-
 /** @returns all employees */
 export const getEmployees = async () => {
   const sql = `select * from employees`;
@@ -37,20 +22,39 @@ export const getEmployee = async (id) => {
   return rows[0];
 };
 
+/** @returns the employee created according to the provided details */
+export const createEmployee = async ({ name, birthday, salary }) => {
+  const sql = `
+    insert into employees(name, birthday, salary)
+    values ($1, $2, $3)
+    returning *
+  `;
+  const values = [name, birthday, salary];
+  const res = await db.query(sql, values);
+
+  return res.rows[0];
+};
+
 /**
  * @returns the updated employee with the given id
  * @returns undefined if employee with the given id does not exist
  */
 export const updateEmployee = async ({ id, name, birthday, salary }) => {
+  const employee = await getEmployee(id);
+
+  if (!employee) return undefined;
+
   const sql = `
     update employees
-    set name = $1, birthday = $2, salary = $3
-    where id = ${id}
+    set name = $2, birthday = $3, salary = $4
+    where id = $1
   `;
-  const values = [name, birthday, salary];
+  const values = [id, name, birthday, salary];
   const res = await db.query(sql, values);
 
-  return res;
+  console.log(employee);
+
+  return employee;
 };
 
 /**
@@ -58,5 +62,12 @@ export const updateEmployee = async ({ id, name, birthday, salary }) => {
  * @returns undefined if employee with the given id does not exist
  */
 export const deleteEmployee = async (id) => {
-  // TODO
+  const deletedEmployee = await getEmployee(id);
+  const sql = `
+    delete from employees
+    where id = ${id}
+  `;
+  db.query(sql);
+
+  return deletedEmployee;
 };
